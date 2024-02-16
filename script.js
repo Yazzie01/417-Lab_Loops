e44aa423da6a09754d2c024825fe7d5f7c4e822e
Yazzie01 <122305485+Yazzie01@users.noreply.github.com>
2024-02-16 15:04:03 -0700

    Adjusting code
    Currently working on console output

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-function drawTriangle(triangleSize) {
-
-   // Your solution goes here
-   
+function drawTriangle(numRows, numCols, boxChar) {
+   for (let i = 0; i < numRows; i++){
+      let boxChar = "";
+      for (let j = 0; j <  numCols; j++){
+         boxChar = "*";
+      }
+   }
+   console.log(boxChar);
 }
+
+drawTriangle(4, 4);
